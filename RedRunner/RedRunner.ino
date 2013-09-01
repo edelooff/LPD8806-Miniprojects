4f894aa113c93277b77a46bdc49b4d66cf4ab980
Elmer de Looff <elmer.delooff@gmail.com>
2013-09-01 16:51:44 +0200

    Added teleportation functionality.
    Every few seconds, a pair of blue and orange teleport dots open up. When the trail hits either portal, it gets transported to the other end of it and continues from there.

--- RedRunner/RedRunner.ino
+++ RedRunner/RedRunner.ino
@@ -6,20 +6,23 @@
 const unsigned int
   ledCount = 96,
   stepInterval = 10,
-  trailLevels[] = {
-      127, 106, 88, 72, 58, 46, 36, 27, 20, 14, 10, 6, 4, 2, 1, 0},
+  trailLevels[] = {127, 106, 88, 72, 58, 46, 36, 27, 20, 14, 10, 6, 4, 2, 1, 0},
   trailLength = sizeof(trailLevels) / sizeof(int);
-unsigned int trail[trailLength] = {};
+int trail[trailLength] = {};
 LPD8806 strip = LPD8806(ledCount); // Hardware SPI. Pins 11 & 13 on Arduino Uno.
 
 // Barrier variables
 const unsigned int
   barrierChance = 40,
-  barrierInterval = 600,
+  barrierInterval = 550,
   barrierLevels[] = {6, 22, 56, 127},
   barrierStartLevel = sizeof(barrierLevels) / sizeof(int),
   maxBarriers = 20;
 barrier_t barriers[maxBarriers];
+
+// Teleport variables
+const unsigned int teleportInterval = 2700;
+int teleport[] = {-1, -1};
 
 void setup() {
   randomSeed(analogRead(0));
@@ -29,7 +32,8 @@ void setup() {
 void loop() {
   static long
     nextBarrier = 0,
-    nextStep = 0;
+    nextStep = 0,
+    nextTeleport = teleportInterval;
   long currentMillis = millis();
   if (currentMillis >= nextStep) {
     nextStep = currentMillis + stepInterval;
@@ -38,6 +42,10 @@ void loop() {
   if (currentMillis >= nextBarrier) {
     nextBarrier = currentMillis + barrierInterval;
     placeBarrier();
+  }
+  if (currentMillis >= nextTeleport) {
+    nextTeleport = currentMillis + teleportInterval;
+    placeTeleport();
   }
   drawScene();
 }
@@ -49,6 +57,10 @@ void drawScene() {
     barrier_t bar = barriers[i];
     if (bar.level)
       strip.setPixelColor(bar.position, 0, 0, barrierLevels[bar.level - 1]);
+  }
+  if (teleport[0] != -1) {
+    strip.setPixelColor(teleport[0], 0, 127, 0);
+    strip.setPixelColor(teleport[1], 127, 0, 22);
   }
   strip.show();
 }
@@ -69,23 +81,56 @@ int firstNonBarrier() {
 
 void moveTrail(void) {
   static char dir = 1;
-  int bar = barrierCollision((trail[0] + ledCount + dir) % ledCount);
-  if (bar >= 0) {
-    if (--barriers[bar].level == 0) // Barrier integrity reduces at collision.
-      strip.setPixelColor(barriers[bar].position, 0); // Remove broken barrier.
-    dir *= -1;                      // Reverse direction after collision
-  }  for (int i = trailLength; i-- > 1;)
+  int bar, teleportLocation;
+  teleportLocation = teleportFrom(trail[0]);
+  for (int i = trailLength; i-- > 1;)
     trail[i] = trail[i - 1];
-  trail[0] = (trail[1] + ledCount + dir) % ledCount;
+  if (teleportLocation >= 0) {
+    trail[0] = teleportLocation;
+  } else {
+    bar = barrierCollision((trail[1] + ledCount + dir) % ledCount);
+    if (bar >= 0) {
+      if (--barriers[bar].level == 0) // Barrier integrity reduces at collision.
+        strip.setPixelColor(barriers[bar].position, 0); // Remove broken barrier.
+      dir *= -1;                      // Reverse direction after collision
+    }
+    trail[0] = (trail[1] + ledCount + dir) % ledCount;
+  }
+}
+
+unsigned int randomFreeIndex() {
+  unsigned int index = random(ledCount);
+  while (barrierCollision(index) > 0 ||
+         index == trail[0] || index == teleport[0] || index == teleport[1])
+    index = random(ledCount);
+  return index;
 }
 
 void placeBarrier() {
   if (random(100) < barrierChance) {
     int availableIndex = firstNonBarrier();
-    if (availableIndex >= 0) {
-      int barrierPosition = random(ledCount);
-      if (barrierCollision(barrierPosition) < 0 && barrierPosition != trail[0])
-        barriers[availableIndex] = {barrierPosition, barrierStartLevel};
-    }
+    if (availableIndex >= 0)
+      barriers[availableIndex] = {randomFreeIndex(), barrierStartLevel};
   }
+}
+
+void placeTeleport() {
+  if (teleport[0] < 0) {
+    teleport[0] = randomFreeIndex();
+    teleport[1] = randomFreeIndex();
+  }
+}
+
+int teleportFrom(unsigned int start) {
+  int retval = -1;
+  if (start == teleport[0]) {
+    retval = teleport[1];
+    teleport[0] = -1;
+    teleport[1] = -1;
+  } else if (start == teleport[1]) {
+    retval = teleport[0];
+    teleport[0] = -1;
+    teleport[1] = -1;
+  }
+  return retval;
 }
